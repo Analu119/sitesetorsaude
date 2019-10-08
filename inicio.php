877ef45975f38a48039273d369955d801cfc55f0
Analu119 <56238330+Analu119@users.noreply.github.com>
2019-10-08 12:25:45 -0300

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Ana Luiza, Debora">
+    <meta name="description" content="Site para gerenciar atendimentos da Enfermaria.">
+    <meta name="keywords" content="saúde, bem-estar">
+    <title>Enfermaria</title>
+    <link rel="icon" href="assets/img/icon.jpg">
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<img src="assets/img/brasao.jpg" width="15%" padding="3%">
+<header>
+
+<h6>MINISTÉRIO DA EDUCAÇÃO</h6>
+<h6>SECRETARIA DE EDUCAÇÃO PROFISSIONAL E TECNOLÓGICA</h6>
+<h6>INSTITUTO FEDERAL DE EDUCAÇÃO, CIÊNCIA E TECNOLOGIA DO TOCANTINS</h6>
+<h6>CAMPUS DIANÓPOLIS</h6>
+<h6>GERÊNCIA DE ENSINO</h6>
+<h6>COORDENAÇÃO DE ASSISTÊNCIA ESTUDANTIL</h6>
+<h6>SETOR DE ASSISTÊNCIA À SAÚDE</h6>
+</header>
+<body>
+
+<div class="menu">
+        <a href="index.php" >Cadastrar Pacientes </a>       
+</div>
+<div class="menu">           
+<a href="listar.php">Acervo de Dados</a> </div>
+
+<div>
+
+<p>O <b>Setor de Assistência á Saúde</b> tem a missão de oferecer atenção básica à saúde para os alunos regularmente matriculados no IFTO, bem como os servidores,
+     por meio de uma equipe multidisciplinar composta por duas enfermeiras e demais setores de assistência ao aluno, tendo em vista a promoção, proteção e recuperação da saúde dos estudantes, por meio de atendimentos
+      ambulatoriais e encaminhamentos, tendo esse zelo em saúde.</p>
+
+</div>
+
+
+<footer>
+   <img class="imgif"src="assets/img/if.png">
+   <div> Loteamento Rio Palmeira. Lt 1 </div>
+   <div> 77300-000 Dianópolis - TO </div>
+   <div><a href="http://dianopolis.ifto.edu.br">www.dianopolis.ifto.edu.br</a></div> 
+   <div>cae.dianopolis@ifto.edu.br</div>
+</footer>
+
+</body>
+
+</html>
+<script src="vendor/popper.min.js"></script>
+    <script src="vendor/jquery-3.3.1.slim.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
